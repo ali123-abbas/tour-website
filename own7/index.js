b68e42ba23ad9f8943a3a40e03f7acbe64bbee21
Ali Abbas <78821584+ali123-abbas@users.noreply.github.com>
2022-09-02 05:32:15 -0700

    Add files via upload
    Website About bali adventure

---
+++ own7/index.js
@@ -0,0 +1,19 @@
+const nav = document.querySelector('nav');
+const header =document.querySelector('header');
+
+const options = {
+    rooMargin: '-12%'
+}
+
+const observer = new IntersectionObserver (entries => {
+     entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            nav.classList.remove('changeColor')
+        } else {
+            nav.classList.add('changeColor')
+
+        }
+     })
+}, options)
+
+observer.observe(header)
